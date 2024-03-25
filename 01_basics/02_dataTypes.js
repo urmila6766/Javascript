764c63f70855b78da1ea87a0cf2e4358d6919567
urmila6766 <114945856+urmila6766@users.noreply.github.com>
2024-03-25 11:00:07 +0000

    DataType

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict" //Treate  all js code in newer version
+
+//alert(3 +3) we r using node.js not browser
+
+console.log(3+3);// code realibity should be high
+
+console.log("Urmila");
+
+// Important notes Datatype
+
+// null => Null is object or standalone value
+
+//undefined=>
+//symbol=> unique
+
+  
+let bankName;  
+let age=3   //number => 2 powar 53
+let idValidate=true // boolean
+let name="Urmi" // string
+let number=1222222222222222222290877779979   //bigInt
+console.log(typeof null);   
+
+
+
+
+
